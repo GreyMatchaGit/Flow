a90c1fad4af37fd6a367da93e1a9f0889fb2965c
greymatcha <greymatcha@proton.me>
2024-12-15 08:15:38 +0800

    Added Color Palette class

--- src/main/java/com/greymatcha/todolist/fxmlcontroller/SideNavbarController.java
+++ src/main/java/com/greymatcha/todolist/fxmlcontroller/SideNavbarController.java
@@ -1,5 +1,6 @@
 package com.greymatcha.todolist.fxmlcontroller;
 
+import com.greymatcha.todolist.utils.ColorPalette;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,26 +13,22 @@ import java.util.ResourceBundle;
 public class SideNavbarController implements Initializable {
 
     @FXML
-    Button inboxButton, todayButton;
+    Button inboxButton, todayButton, userButton;
 
     @FXML
-    Rectangle inboxBackground, todayBackground;
+    Rectangle inboxBackground, todayBackground, userBackground;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         buttonHoverEffect(inboxButton, inboxBackground);
         buttonHoverEffect(todayButton, todayBackground);
+        buttonHoverEffect(userButton, userBackground);
     }
 
     public void buttonHoverEffect(Button button, Rectangle background) {
 
-        button.setOnMouseEntered(_ -> {
-            background.setFill(Color.web("#ffffff"));
-        });
-
-        button.setOnMouseExited(_ -> {
-            background.setFill(Color.web("#f8f4f4"));
-        });
+        button.setOnMouseEntered(_ -> background.setFill(Color.web(ColorPalette.light)));
+        button.setOnMouseExited(_ -> background.setFill(Color.web(ColorPalette.medium)));
     }
 }
---
+++ src/main/java/com/greymatcha/todolist/utils/ColorPalette.java
@@ -0,0 +1,8 @@
+package com.greymatcha.todolist.utils;
+
+public class ColorPalette {
+
+    public static final String light = "#ffffff";
+    public static final String medium = "#f8f4f4";
+    public static final String dark = "";
+}
