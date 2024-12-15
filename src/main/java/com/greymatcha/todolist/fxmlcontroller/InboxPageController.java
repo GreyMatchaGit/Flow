5bc10d9ad7fec0de7244487ddc7374796bcef0af
greymatcha <greymatcha@proton.me>
2024-12-15 20:37:11 +0800

    Adding task pane now opens with animation.

--- src/main/java/com/greymatcha/todolist/fxmlcontroller/InboxPageController.java
+++ src/main/java/com/greymatcha/todolist/fxmlcontroller/InboxPageController.java
@@ -1,10 +1,17 @@
 package com.greymatcha.todolist.fxmlcontroller;
 
 import com.greymatcha.todolist.utils.CustomAnimation;
+import javafx.animation.ScaleTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.BoxBlur;
+import javafx.scene.effect.GaussianBlur;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,12 +23,40 @@ public class InboxPageController implements Initializable {
     Button addTaskButton;
 
     @FXML
-    Pane addTaskParentPane;
+    Circle taskPaneCloseButton;
+
+    @FXML
+    Pane taskPane, addTaskParentPane, contentPane;
+
+    @FXML
+    TextField taskNameField, taskDescriptionField;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        taskPane.setScaleX(0);
+        taskPane.setScaleY(0);
+
+        taskNameField.setBackground(null);
+        taskDescriptionField.setBackground(null);
+
         CustomAnimation.buttonClickEffect(addTaskButton, addTaskParentPane);
+
+        addTaskButton.setOnMouseClicked(_ -> {
+
+            contentPane.setEffect(new GaussianBlur());
+            taskNameField.clear();
+            taskDescriptionField.clear();
+            CustomAnimation.createScaleTransition(taskPane, 1.0, Duration.millis(100)).play();
+        });
+
+        taskPaneCloseButton.setOnMouseClicked(_ -> {
+
+            contentPane.setEffect(null);
+            CustomAnimation.createScaleTransition(taskPane, 0.0, Duration.millis(100)).play();
+        });
+
+        taskPaneCloseButton.setCursor(Cursor.HAND);
     }
 }
--- src/main/java/com/greymatcha/todolist/utils/CustomAnimation.java
+++ src/main/java/com/greymatcha/todolist/utils/CustomAnimation.java
@@ -4,7 +4,6 @@ import javafx.animation.Interpolator;
 import javafx.animation.ScaleTransition;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -12,23 +11,23 @@ import javafx.util.Duration;
 
 public class CustomAnimation {
 
-    public static void buttonHoverEffect(Button button, Rectangle background) {
+    public static void buttonHoverEffect(Node button, Rectangle background) {
 
         button.setCursor(Cursor.HAND);
         button.setOnMouseEntered(_ -> background.setFill(Color.web(ColorPalette.light)));
         button.setOnMouseExited(_ -> background.setFill(Color.web(ColorPalette.medium)));
     }
 
-    public static void buttonClickEffect(Button button, Pane parentPane) {
+    public static void buttonClickEffect(Node button, Pane parentPane) {
 
         button.setCursor(Cursor.HAND);
-        button.setOnMousePressed(_ -> createScaleTransition(parentPane, 0.95).play());
-        button.setOnMouseReleased(_ -> createScaleTransition(parentPane, 1.0).play());
+        button.setOnMousePressed(_ -> createScaleTransition(parentPane, 0.95, Duration.millis(50)).play());
+        button.setOnMouseReleased(_ -> createScaleTransition(parentPane, 1.0, Duration.millis(50)).play());
     }
 
-    public static ScaleTransition createScaleTransition(Node node, Double scalingFactor) {
+    public static ScaleTransition createScaleTransition(Node node, Double scalingFactor, Duration duration) {
 
-        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(50), node);
+        ScaleTransition scaleTransition = new ScaleTransition(duration, node);
         scaleTransition.setToX(scalingFactor);
         scaleTransition.setToY(scalingFactor);
         scaleTransition.setInterpolator(Interpolator.EASE_IN);
